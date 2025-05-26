3820c439caa92a69c1d3a36f4485d887da16e32d
KBstos <kauebastos74@gmail.com>
2025-05-26 11:38:13 -0300

    feat: add page for user not found where a user want to acess news

--- src/modules/newsPage/screen/NewsScreen.tsx
+++ src/modules/newsPage/screen/NewsScreen.tsx
@@ -17,11 +17,15 @@ import { NewsInterface } from "../../../shared/listNews/listNews";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
+import { useUserRole } from "../../../authentication/useUserRole";
+import { ContainerHeader, Container } from "../../home/styles/homeStyle";
+import { HomeLink } from "../../errorPage/styles/errorPage.styles";
 
 function NewsPage() {
   const [news, setNews] = useState<NewsInterface | null>(null);
   const [error, setError] = useState("");
   const { id } = useParams();
+  const userRole = useUserRole();
 
   useEffect(() => {
     const fetchData = async () => {
@@ -55,46 +59,72 @@ function NewsPage() {
     return <div>News not found</div>;
   }
 
+  if (userRole) {
+    return (
+      <Body>
+        <HeaderNews>
+          <ProfileIconNews>
+            <MenuProfile />
+          </ProfileIconNews>
+          <a href="/">
+            <Logo src={LogoImg} style={{ width: "80px", height: "80px" }} />
+          </a>
+        </HeaderNews>
+        <SectionNews>
+          <NewsImage src={news.src} />
+          <DateNews>
+            {new Date(news.created_at).toLocaleDateString("pt-BR", {
+              day: "2-digit",
+              month: "long",
+              year: "numeric",
+            })}
+          </DateNews>
+          <TitleNews
+            style={{
+              padding: "20px 2px",
+              fontSize: "1.2rem",
+              fontWeight: "bold",
+            }}
+          >
+            {news.title}
+          </TitleNews>
+          <NewsContent
+            style={{
+              textAlign: "justify",
+              hyphens: "auto",
+              padding: "10px",
+              whiteSpace: "pre-line",
+            }}
+          >
+            {news.content}
+          </NewsContent>
+        </SectionNews>
+      </Body>
+    );
+  }
+
   return (
-    <Body>
-      <HeaderNews>
-        <ProfileIconNews>
-          <MenuProfile />
-        </ProfileIconNews>
-        <a href="/">
-          <Logo src={LogoImg} style={{ width: "80px", height: "80px" }} />
-        </a>
-      </HeaderNews>
-      <SectionNews>
-        <NewsImage src={news.src} />
-        <DateNews>
-          {new Date(news.created_at).toLocaleDateString("pt-BR", {
-            day: "2-digit",
-            month: "long",
-            year: "numeric",
-          })}
-        </DateNews>
-        <TitleNews
-          style={{
-            padding: "20px 2px",
-            fontSize: "1.2rem",
-            fontWeight: "bold",
-          }}
-        >
-          {news.title}
-        </TitleNews>
-        <NewsContent
-          style={{
-            textAlign: "justify",
-            hyphens: "auto",
-            padding: "10px",
-            whiteSpace: "pre-line",
-          }}
-        >
-          {news.content}
-        </NewsContent>
-      </SectionNews>
-    </Body>
+    <>
+      <Body style={{maxHeight: '100vh'}}>
+        <Container>
+          <ContainerHeader
+            style={{ display: "flow", textAlign: "center"}}
+          >
+            <Logo src={LogoImg} />
+            <h2 style={{ color: "white", fontSize: "1.4rem", margin: "50px" }}>
+              User not found, please login to acess the news
+            </h2>
+            <HomeLink
+              style={{ maxWidth: "120px", margin: "auto", marginTop: "40px" }}
+            >
+              <a href="/login" style={{ textDecoration: "none", color: "white" }}>
+                Login
+              </a>
+            </HomeLink>
+          </ContainerHeader>
+        </Container>
+      </Body>
+    </>
   );
 }
 
